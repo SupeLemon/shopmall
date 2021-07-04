7d9b904b41389d4a84406361406b39bf17099f1c
SuperLemon <2285065364@qq.com>
2021-07-04 16:02:28 +0800

    上传代码

---
+++ src/views/home/childComps/RecommendView.vue
@@ -0,0 +1,44 @@
+<template>
+  <ul class="recommend-view">
+      <li v-for="item in recommends">
+          <a :href="item.link">
+              <img :src="item.image" alt="">
+              <div>{{item.title}}</div>
+          </a>
+      </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: "RecommendView",
+  props: {
+      recommends: {
+          type: Array,
+          default() {
+              return []
+          }
+      }
+  }
+}
+</script>
+
+<style scoped>
+    .recommend-view {
+        display: flex;
+        width: 100%;
+        text-align: center;
+        padding: 10px 0 20px;
+        border-bottom: 8px solid #eee;
+        font-size: 14px;
+    }
+    li {
+        list-style: none;
+        flex: 1;
+    }
+    li img {
+        width: 70px;
+        height: 70px;
+        margin-bottom: 10px;
+    }
+</style>
